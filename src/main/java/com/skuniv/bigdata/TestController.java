d783323bb9d02333d5560301af5c692aa60ff82b
geonyoungkim <c2619zz@naver.com>
2018-11-08 22:50:42 +0900

    application.yml 업데이트

---
+++ src/main/java/com/skuniv/bigdata/TestController.java
@@ -0,0 +1,13 @@
+package com.skuniv.bigdata;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
