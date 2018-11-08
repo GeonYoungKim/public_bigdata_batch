77425a4207f9b4c1acc49fa3ad290c4d54ec58fc
geonyoungkim <c2619zz@naver.com>
2018-11-08 23:28:41 +0900

    jenkins 배포 테스트

--- src/main/java/com/skuniv/bigdata/TestController.java
+++ src/main/java/com/skuniv/bigdata/TestController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-
-        
         return "test";
     }
 }
