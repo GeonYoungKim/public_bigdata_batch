76e84431a7c0b7bb296d1c6b21ccd60f78df5d61
GEONYOUNGKIM <c2619zz@naver.com>
2019-02-16 16:56:24 +0900

    no message

--- src/main/java/com/skuniv/bigdata/BigDataApplication.java
+++ src/main/java/com/skuniv/bigdata/BigDataApplication.java
@@ -1,8 +1,11 @@
 package com.skuniv.bigdata;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,21 +15,26 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.List;
 
+@Slf4j
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BigDataApplication implements ApplicationRunner {
-    private static ConfigurableApplicationContext ctx;
+    @Autowired
+    ConfigurableApplicationContext configurableApplicationContext;
+    @Autowired
+    private JobLauncher jobLauncher;
 
     public static void main(String[] args) {
-        ctx = SpringApplication.run(BigDataApplication.class, args);
+        SpringApplication.run(BigDataApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<String> job = args.getOptionValues("job");
-        System.out.println("job => " + job.get(0));
-        System.out.println("ctx => " + ctx);
-        JobLauncher jobLauncher = (JobLauncher) ctx.getBean("jobLauncher");
-        Job job1 = (Job) ctx.getBean(job.get(0));
-        jobLauncher.run(job1, new JobParameters());
+        log.warn("job => " + job.get(0));
+        log.warn("ctx => " + configurableApplicationContext);
+        Job job1 = (Job) configurableApplicationContext.getBean(job.get(0));
+        log.warn("job1 => {}", job1.getName());
+        JobParametersBuilder builder = new JobParametersBuilder();
+        jobLauncher.run(job1, builder.toJobParameters());
     }
 }
