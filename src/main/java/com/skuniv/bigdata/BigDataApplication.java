9aa8d9ad642b395c220bd418774715ee06c274e3
GEONYOUNGKIM <c2619zz@naver.com>
2019-02-16 16:49:43 +0900

    no message

--- src/main/java/com/skuniv/bigdata/BigDataApplication.java
+++ src/main/java/com/skuniv/bigdata/BigDataApplication.java
@@ -1,24 +1,30 @@
 package com.skuniv.bigdata;
 
-import com.skuniv.bigdata.domain.dto.BargainOpenApiDto;
-import com.skuniv.bigdata.domain.dto.open_api.BargainItemDto;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.List;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class BigDataApplication {
-
+public class BigDataApplication implements ApplicationRunner {
+    private static ConfigurableApplicationContext ctx;
     public static void main(String[] args) {
-        SpringApplication.run(BigDataApplication.class, args);
+        ctx = SpringApplication.run(BigDataApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> job = args.getOptionValues("job");
+        JobLauncher jobLauncher = (JobLauncher) ctx.getBean("jobLauncher");
+        Job job1 = (Job) ctx.getBean(job.get(0));
+        jobLauncher.run(job1,new JobParameters());
     }
 }
